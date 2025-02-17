e7625a0b364da95b77a209a04ce8879b710db77c
dvanosdall <dvanosdall15@gmail.com>
2025-02-17 09:48:19 -0600

    Activity 6
    broke out the main code to be properly modularized.

--- js/main.js
+++ js/main.js
@@ -9,11 +9,25 @@
 
 // Wait until the webpage is fully loaded before running the script
 document.addEventListener("DOMContentLoaded", function () {
-    // Initialize the map - Centered at the equator, zoom level 2
-    // I went with this view since my earthquakes are all around the world
-    const mymap = L.map('mymap').setView([20, 0], 2); 
+    // Initialize the map
+    const mymap = initializeMap();
 
     // Add tile layer from OpenStreetMap to the map
+    addTileLayer(mymap);
+
+    // Fetch GeoJSON data from the 'data' folder
+    fetchGeoJSONData(mymap);
+});
+
+// Initialize the map
+function initializeMap() {
+    // Centered at the equator, zoom level 2
+    // I went with this view since my earthquakes are all around the world
+    return L.map('mymap').setView([20, 0], 2);
+}
+
+// Add tile layer from OpenStreetMap to the map
+function addTileLayer(mymap) {
     // added this tileset https://leaflet-extras.github.io/leaflet-providers/preview/#filter=Stadia.AlidadeSmoothDark  
     // this helps make the eq's pop
     L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
@@ -22,8 +36,10 @@ document.addEventListener("DOMContentLoaded", function () {
         attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         ext: 'png'
     }).addTo(mymap);
+}
 
-    // Fetch GeoJSON data from the 'data' folder
+// Fetch GeoJSON data from the 'data' folder
+function fetchGeoJSONData(mymap) {
     fetch('./data/all_month.geojson')
         .then(response => {
             if (!response.ok) {
@@ -34,44 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(data => {
             const top100Features = data.features.slice(0, 100);
             console.log('Top 100 features:', top100Features);
-            L.geoJSON(top100Features, {
-                // Customize the point style based on earthquake magnitude
-                pointToLayer: (feature, latlng) => {
-                    const mag = feature.properties.mag;
-                    const color = mag > 5 ? 'red' : mag > 3 ? 'orange' : 'yellow';
-
-                    // Create a circle marker with properties based on the magnitude
-                    return L.circleMarker(latlng, {
-                        // Scale by magnitude
-                        radius: mag * 2, 
-                        // Color based on magnitude
-                        fillColor: color,
-                        // Border color
-                        color: "#000",
-                        // Border weight
-                        weight: 1,
-                        // Border opacity
-                        opacity: 1,
-                        // Fill opacity
-                        fillOpacity: 0.8
-                    });
-                },
-                // Add popup to each feature showing earthquake details
-                // there is a bunch of info so we are going to dsiplay the important eq data 
-                // the more info is really neat since the usgs has the url in teh data set
-                onEachFeature: (feature, layer) => {
-                    if (feature.properties) {
-                        layer.bindPopup(
-                           `<strong>Location:</strong> ${feature.properties.place}<br>
-                             <strong>Magnitude:</strong> ${feature.properties.mag}<br>
-                             <strong>Depth:</strong> ${feature.geometry.coordinates[2]} km<br>
-                             <strong>Time:</strong> ${new Date(feature.properties.time).toLocaleString()}<br>
-                             <a href="${feature.properties.url}" target="_blank">More Info</a>`
-                        );
-                    }
-                }
-                // Add the GeoJSON layer to the map
-            }).addTo(mymap);
+            addGeoJSONLayer(mymap, top100Features);
         })
         .catch(error => {
             // Handle errors related to loading the GeoJSON file
@@ -81,4 +60,45 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.error('An Error loading the GeoJSON data has occurred:', error);
             }
         });
-});
+}
+
+// Add GeoJSON layer to the map
+function addGeoJSONLayer(mymap, features) {
+    L.geoJSON(features, {
+        // Customize the point style based on earthquake magnitude
+        pointToLayer: (feature, latlng) => {
+            const mag = feature.properties.mag;
+            const color = mag > 5 ? 'red' : mag > 3 ? 'orange' : 'yellow';
+
+            // Create a circle marker with properties based on the magnitude
+            return L.circleMarker(latlng, {
+                // Scale by magnitude
+                radius: mag * 2,
+                // Color based on magnitude
+                fillColor: color,
+                // Border color
+                color: "#000",
+                // Border weight
+                weight: 1,
+                // Border opacity
+                opacity: 1,
+                // Fill opacity
+                fillOpacity: 0.8
+            });
+        },
+        // Add popup to each feature showing earthquake details
+        // there is a bunch of info so we are going to dsiplay the important eq data 
+        // the more info is really neat since the usgs has the url in teh data set
+        onEachFeature: (feature, layer) => {
+            if (feature.properties) {
+                layer.bindPopup(
+                    `<strong>Location:</strong> ${feature.properties.place}<br>
+                     <strong>Magnitude:</strong> ${feature.properties.mag}<br>
+                     <strong>Depth:</strong> ${feature.geometry.coordinates[2]} km<br>
+                     <strong>Time:</strong> ${new Date(feature.properties.time).toLocaleString()}<br>
+                     <a href="${feature.properties.url}" target="_blank">More Info</a>`
+                );
+            }
+        }
+    }).addTo(mymap);
+}
